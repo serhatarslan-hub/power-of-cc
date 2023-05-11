5966541c16663f182303ec5be4c736809050e62b
Serhat Arslan <sarslan@stanford.edu>
2023-05-11 12:56:43 -0700

    Update the experiment script to collect more data

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ REPEAT_CNT=20
 IPERF_DST="10.0.0.1"
 IPERF_IFACE="bond0"
 IPERF_SIZE="50G"
-MTU_SIZES=("1500" "9000")
+MTU_SIZES=("1500" "3000" "6000" "9000")
 
 # Determine the experiment ID
 EXP_ID=( $(date +"%y%m%d-%H%M") )
@@ -14,19 +14,20 @@ if [ ! -d "$EXP_DIR" ]; then
 else
     echo "The data directory for this experiment already exists!"
 fi
-EXP_FILE=$EXP_DIR"/energy.log"
+ENERGY_FILE=$EXP_DIR"/energy.log"
 
 # Measure energy spent for UDP traffic
 for MTU_SIZE in ${MTU_SIZES[@]}; do
     sudo ifconfig $IPERF_IFACE mtu $MTU_SIZE up
     for CNT in $(seq $REPEAT_CNT); do
+        JSON_FILE=$EXP_DIR"/udp_${MTU_SIZE}_${CNT}.json"
         NRG_0=( $(sudo cat /sys/class/powercap/intel-rapl/intel-rapl\:0/energy_uj))
         NRG_1=( $(sudo cat /sys/class/powercap/intel-rapl/intel-rapl\:1/energy_uj))
-        iperf3 -c $IPERF_DST -n $IPERF_SIZE -b 0 -u -i 60
+        iperf3 -c $IPERF_DST -n $IPERF_SIZE -b 0 -u -i 60 --json > $JSON_FILE
         NRG_0=$(($(sudo cat /sys/class/powercap/intel-rapl/intel-rapl\:0/energy_uj)-NRG_0))
         NRG_1=$(($(sudo cat /sys/class/powercap/intel-rapl/intel-rapl\:1/energy_uj)-NRG_1))
         NRG=$((NRG_0+NRG_1))
-        echo "cc=udp mtu=$MTU_SIZE energy_uJ=${NRG}" >> $EXP_FILE
+        echo "cc=udp mtu=$MTU_SIZE cnt=$CNT energy_uJ=${NRG}" >> $ENERGY_FILE
     done
 done
 
@@ -38,13 +39,14 @@ for CCA in "${CCA_AVAIL[@]}"; do
     for MTU_SIZE in ${MTU_SIZES[@]}; do
         sudo ifconfig $IPERF_IFACE mtu $MTU_SIZE up
         for CNT in $(seq $REPEAT_CNT); do
+            JSON_FILE=$EXP_DIR"/${CCA}_${MTU_SIZE}_${CNT}.json"
             NRG_0=( $(sudo cat /sys/class/powercap/intel-rapl/intel-rapl\:0/energy_uj))
             NRG_1=( $(sudo cat /sys/class/powercap/intel-rapl/intel-rapl\:1/energy_uj))
-            iperf3 -c $IPERF_DST -n $IPERF_SIZE -i 60
+            iperf3 -c $IPERF_DST -n $IPERF_SIZE -i 60 --json > $JSON_FILE
             NRG_0=$(($(sudo cat /sys/class/powercap/intel-rapl/intel-rapl\:0/energy_uj)-NRG_0))
             NRG_1=$(($(sudo cat /sys/class/powercap/intel-rapl/intel-rapl\:1/energy_uj)-NRG_1))
             NRG=$((NRG_0+NRG_1))
-            echo "cc=$CCA mtu=$MTU_SIZE energy_uJ=${NRG}" >> $EXP_FILE
+            echo "cc=$CCA mtu=$MTU_SIZE cnt=$CNT energy_uJ=${NRG}" >> $ENERGY_FILE
         done
     done
 done
